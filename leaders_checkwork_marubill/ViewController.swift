f276f0ff44107e6e819c68ebbdab6e1e527e383e
中山弘瑛 <hiroaki1207.gk@gmail.com>
2018-04-15 15:28:27 +0900

    Initial Commit

---
+++ leaders_checkwork_marubill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  leaders_checkwork_marubill
+//
+//  Created by 中山弘瑛 on 2018/04/15.
+//  Copyright © 2018年 中山弘瑛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
